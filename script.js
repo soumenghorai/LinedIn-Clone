64b55487dc3bf0761c7f01f178c4b0dabb20fa01
Soumen Ghorai <soumenghorai2802@gmail.com>
2023-04-22 17:57:32 +0530

    First Commit

---
+++ script.js
@@ -0,0 +1,19 @@
+let profileMenu = document.getElementById("profileMenu");
+
+
+function toggleMenu(){
+    profileMenu.classList.toggle("open-memu");
+}
+let sideActivity = document.getElementById("sidebarActivity");
+let moreLink = document.getElementById("showMoreLink");
+
+function toggleActivity(){
+  sideActivity.classList.toggle("open-activity");
+
+  if(sideActivity.classList.contains("open-activity")){
+    moreLink.innerHTML = "Show less <b>-</b>";
+  }
+  else{
+    moreLink.innerHTML = "Show more <b>+</b>"
+  }
+}
